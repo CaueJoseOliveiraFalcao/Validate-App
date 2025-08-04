8232adb09f042addbc07952855c3d94be05103c2
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2025-08-04 12:19:25 -0300

    update

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
- //runApp(const MyApp());
+  runApp(const MyApp());
 
- runApp(
-  MaterialApp()
- );
+  /*runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.lightBlue,
+            leading: Icon(Icons.menu),
+            title: Text('Validator App'),
+            actions: [
+                Icon(Icons.favorite),
+                Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 16) , child: Icon(Icons.search),),
+                Icon(Icons.more_vert),
+            ],
+        ),
+
+        body: Center(
+          child: Text(
+            'oi',
+            style: TextStyle(fontSize: 50, color: Colors.black),
+          ),
+        ),
+      ),
+    ),
+  );
+*/
+
+}
+
+
+class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return 
+        MaterialApp(
+            home: Scaffold(
+                appBar: AppBar(
+                    backgroundColor: Colors.lightBlue,
+                    leading: Icon(Icons.menu),
+                    title: Text('Validator App'),
+                    actions: [
+                        Icon(Icons.favorite),
+                        Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 16) , child: Icon(Icons.search),),
+                        Icon(Icons.more_vert),
+                    ],
+                ),
+
+                body: Center(
+                child: Text(
+                    'oi',
+                    style: TextStyle(fontSize: 50, color: Colors.black),
+                ),
+                ),
+            ),
+            );
+  }
 }
