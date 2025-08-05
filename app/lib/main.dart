1404e24d5a34a4c90d2e5e260e02bdad46dfc196
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2025-08-05 13:13:48 -0300

    wef

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,62 +1,79 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
 
-  /*runApp(
-    MaterialApp(
+void main(){
+  const String nome = 'comedia';
+  runApp(MyApp(nome : nome));
+}
+
+class MyApp extends StatefulWidget{
+  final String nome;
+  const MyApp({super.key , this.nome = ''});
+
+  @override
+  MyappState createState() => MyappState();
+  }
+
+
+class MyappState extends State<MyApp> {
+  int stalario = 9000;
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+
+        body: Center(child:
+         GestureDetector(
+          onTap: () {print('cricou');},
+          child : Text('o salario de ${widget.nome} e $stalario',)
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+/*void main() {
+  int valor = 10;
+
+  runApp(MyApp(title: "balick bom bom", valor: valor));
+}
+
+class MyApp extends StatelessWidget {
+  final String title;
+  final int valor;
+  const MyApp({super.key , this.title='' , this.valor = 0});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.lightBlue,
-            leading: Icon(Icons.menu),
-            title: Text('Validator App'),
-            actions: [
-                Icon(Icons.favorite),
-                Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 16) , child: Icon(Icons.search),),
-                Icon(Icons.more_vert),
-            ],
+          backgroundColor: Colors.lightBlue,
+          leading: Icon(Icons.menu),
+          title: Text(title),
+          actions: [
+            Icon(Icons.favorite),
+            Padding(
+              padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
+              child: Icon(Icons.search),
+            ),
+            Icon(Icons.more_vert),
+          ],
         ),
 
         body: Center(
           child: Text(
-            'oi',
+            valor.toString(),
             style: TextStyle(fontSize: 50, color: Colors.black),
           ),
         ),
       ),
-    ),
-  );
-*/
-
-}
-
-
-class MyApp extends StatelessWidget {
-    const MyApp({super.key});
-
-    @override
-  Widget build(BuildContext context) {
-    return 
-        MaterialApp(
-            home: Scaffold(
-                appBar: AppBar(
-                    backgroundColor: Colors.lightBlue,
-                    leading: Icon(Icons.menu),
-                    title: Text('Validator App'),
-                    actions: [
-                        Icon(Icons.favorite),
-                        Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 16) , child: Icon(Icons.search),),
-                        Icon(Icons.more_vert),
-                    ],
-                ),
-
-                body: Center(
-                child: Text(
-                    'oi',
-                    style: TextStyle(fontSize: 50, color: Colors.black),
-                ),
-                ),
-            ),
-            );
+    );
   }
 }
+*/
