536a1da4fca969cc9e9fc07a2679dfa3c83bd7c4
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2025-08-06 12:28:19 -0300

    gh

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,79 +1,37 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
-  const String nome = 'comedia';
-  runApp(MyApp(nome : nome));
+void main() {
+  runApp(Myapp());
 }
 
-class MyApp extends StatefulWidget{
-  final String nome;
-  const MyApp({super.key , this.nome = ''});
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
 
   @override
-  MyappState createState() => MyappState();
-  }
-
-
-class MyappState extends State<MyApp> {
-  int stalario = 9000;
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: Scaffold(
-
-        body: Center(child:
-         GestureDetector(
-          onTap: () {print('cricou');},
-          child : Text('o salario de ${widget.nome} e $stalario',)
-        ),
-      ),
-    );
-  }
+  _MyappState createState() => _MyappState();
 }
 
-
-
-
-
-
-/*void main() {
-  int valor = 10;
-
-  runApp(MyApp(title: "balick bom bom", valor: valor));
-}
-
-class MyApp extends StatelessWidget {
-  final String title;
-  final int valor;
-  const MyApp({super.key , this.title='' , this.valor = 0});
-
+class _MyappState extends State<Myapp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightBlue,
-          leading: Icon(Icons.menu),
-          title: Text(title),
-          actions: [
-            Icon(Icons.favorite),
-            Padding(
-              padding: EdgeInsetsGeometry.symmetric(horizontal: 16),
-              child: Icon(Icons.search),
-            ),
-            Icon(Icons.more_vert),
-          ],
-        ),
-
-        body: Center(
-          child: Text(
-            valor.toString(),
-            style: TextStyle(fontSize: 50, color: Colors.black),
+        appBar: AppBar(title: Text('Validade App')),
+        body: Row (
+          mainAxisAlignment: MainAxisAlignment.spaceAround, 
+        children: [
+          Center(
+            child:Text('My app' , 
+            style: TextStyle(fontSize: 34),),
           ),
+          Center(
+            child:Text('My app' , 
+            style: TextStyle(fontSize: 34),),
+          ),
+          ]
         ),
+        
       ),
     );
   }
 }
-*/
