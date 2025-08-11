caf62897a6dda0c274a0e325e11623283f0f2bbc
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2025-08-11 10:43:22 -0300

    rtr

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,42 +1,27 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatefulWidget {
-  const Myapp({super.key});
+
+class MyApp extends StatefulWidget{
+  const MyApp({super.key});
 
   @override
   _MyappState createState() => _MyappState();
 }
 
-class _MyappState extends State<Myapp> {
+class _MyappState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Stack App'),),
-        body: Stack(
-          
-          children: [
-            Container(
-              width: 400,
-              height: 400,
-              color: Colors.blue,
-            ),
-            Positioned(
-              bottom: 75,
-              left: 75,
-              child: Container(
-                width: 150,
-                height: 150,
-                color: Colors.red,
-              ),
- 
-            )
-          ],
-        ),
+        appBar: AppBar(title: Text('imagens'),),
+        body: Image(image: NetworkImage('https://www.inspireuplift.com/resizer/?image=https://cdn.inspireuplift.com/uploads/images/seller_products/31384/1701339468_DW-16992_Yametekudasai9603.jpg&width=600&height=600&quality=90&format=auto&fit=pad'
+        
+        ), fit: BoxFit.fitWidth, width: double.infinity, height: double.infinity ),
       ),
     );
   }
